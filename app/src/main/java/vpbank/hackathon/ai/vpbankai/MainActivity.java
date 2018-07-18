a589c5bd00305c4e21231dddb8095f2cb306f211
Duc Canh <khongduccanh.315@gmail.com>
2018-07-18 22:35:02 +0700

    initiate project

---
+++ app/src/main/java/vpbank/hackathon/ai/vpbankai/MainActivity.java
@@ -0,0 +1,13 @@
+package vpbank.hackathon.ai.vpbankai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
